89a7bd25d1ebeea127745c55487266e4939fb5a3
sneweep <sneweep@github.com>
2022-03-16 19:36:49 -0400

    updated weather

--- blocks.h
+++ blocks.h
@@ -1,15 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "~/suckless/dwmblocks/statusbar/sb-volume",	0,		10},
+	{"", "~/suckless/dwmblocks/statusbar/sb-volume",     0,     10},
 
-	{"", "~/suckless/dwmblocks/statusbar/sb-cpu",      10,		0},
+	{"", "~/suckless/dwmblocks/statusbar/sb-cpu",        10,    0},
 
-	{"", "~/suckless/dwmblocks/statusbar/sb-battery",      10,		0},
+	{"", "~/suckless/dwmblocks/statusbar/sb-battery",    10,    0},
 
-	{"", "~/suckless/dwmblocks/statusbar/sb-weather",  600,	0},
+	{"", "~/suckless/dwmblocks/statusbar/sb-weather",    600,	0},
 
-	{"", "~/suckless/dwmblocks/statusbar/sb-date",  5,	0},
+	{"", "~/suckless/dwmblocks/statusbar/sb-date",       5,     0},
 
 };
 
