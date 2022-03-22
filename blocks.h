0b47636887eb5cc01e3535bd125879e766e9f438
sneweep <sneweep@github.com>
2022-03-21 21:41:10 -0400

    clickable statusbar

--- blocks.h
+++ blocks.h
@@ -3,13 +3,13 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "~/suckless/dwmblocks/statusbar/sb-volume",     0,     10},
 
-	{"", "~/suckless/dwmblocks/statusbar/sb-cpu",        10,    0},
+	{"", "~/suckless/dwmblocks/statusbar/sb-cpu",        10,    1},
 
-	{"", "~/suckless/dwmblocks/statusbar/sb-battery",    10,    0},
+	{"", "~/suckless/dwmblocks/statusbar/sb-battery",    10,    2},
 
-	{"", "~/suckless/dwmblocks/statusbar/sb-weather",    600,	0},
+	{"", "~/suckless/dwmblocks/statusbar/sb-weather",    600,	3},
 
-	{"", "~/suckless/dwmblocks/statusbar/sb-date",       5,     0},
+	{"", "~/suckless/dwmblocks/statusbar/sb-date",       5,     4},
 
 };
 
